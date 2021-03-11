fd134e7c4668cbafbcef61282f71bfecdab25f48
arundalal2003 <80278756+arundalal2003@users.noreply.github.com>
2021-03-11 21:10:59 +0530

    Update Logo.jsx
    search bar is removed

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -10,11 +10,7 @@ const Logo = () => {
             <a className="navbar-brand" href="#">
                 <img src="Logo2.jpg" width="100" height="auto" alt=""/>
             </a>
-            <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                
-            </form>
+          
             
             <button className="btn btn-outline-success my-2 my-sm-0 " > <AccountCircleIcon className= "fs-2" /> {""}LOGIN</button>
         </nav> 
